0316f7b466c21587403f577fea89c5a6c0d2544c
kazushi-lagoon <kazu0226_7382@yahoo.co.jp>
2020-06-18 17:23:20 +0900

    make function of memo
    メモとしての機能、表示、作成、編集、削除を実装。

---
+++ app.rb
@@ -0,0 +1,59 @@
+require 'sinatra'
+require 'sinatra/reloader'
+
+class Rack::MethodOverride
+  ALLOWED_METHODS=%w[POST GET]
+  def method_override(env)
+    req = Rack::Request.new(env)
+    method = req.params[METHOD_OVERRIDE_PARAM_KEY] || env[HTTP_METHOD_OVERRIDE_HEADER]
+    method.to_s.upcase
+  end
+end
+
+enable :method_override
+
+get '/new_memo' do
+  erb :new_memo
+end
+
+post '/new' do
+  content = params[:content]
+  number = Dir.open('./public').children.count
+  File.open("./public/#{number+1}.txt", 'wb') do |f|
+    f.write(content)
+  end
+  redirect '/'
+end
+
+get '/' do
+  @number = Dir.open('./public').children.count
+  #binding.irb
+  erb :top
+end
+
+get '/edit_memo/:i' do
+  @number = params[:i]
+  @file_content = File.open("./public/#{@number}.txt").read
+  erb :edit_memo
+end
+
+patch '/edit_memo/:i' do
+  number = params[:i]
+  content = params[:content]
+  File.open("./public/#{number}.txt", 'wb') do |f|
+    f.write(content)
+  end
+  redirect '/'
+end
+
+get '/show_memo/:i' do
+  @number = params[:i]
+  @file_content = File.open("./public/#{@number}.txt").read
+  erb :show_memo
+end
+
+delete '/memo/:id' do
+  number = params[:id]
+  File.delete("./public/#{number}.txt")
+  redirect '/'
+end
