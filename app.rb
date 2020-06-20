c4595f195b4def9e58b100e20b026b4255a6f4e9
kazushi-lagoon <kazu0226_7382@yahoo.co.jp>
2020-06-21 06:53:58 +0900

    make oriented_object
    オブジェクト指向プログラムに変更しました。機能は変わっていません。

--- app.rb
+++ app.rb
@@ -2,6 +2,34 @@
 
 require 'sinatra'
 require 'sinatra/reloader'
+require 'pg'
+
+# control memo
+class Memo
+  attr_reader :number
+
+  def initialize(number)
+    @number = number
+  end
+
+  def self.memos_count
+    Dir.open('./public/memos').children.count
+  end
+
+  def memo_content
+    File.open("./public/memos/#{number}.txt").read
+  end
+
+  def content_write(content)
+    File.open("./public/memos/#{number}.txt", 'wb') do |f|
+      f.write(content)
+    end
+  end
+
+  def delete
+    File.delete("./public/memos/#{number}.txt")
+  end
+end
 
 get '/new_memo' do
   erb :new_memo
@@ -9,41 +37,36 @@ end
 
 post '/new' do
   content = params[:content]
-  number = Dir.open('./public/memos').children.count
-  File.open("./public/memos/#{number + 1}.txt", 'wb') do |f|
-    f.write(content)
-  end
+  Memo.new(Memo.memos_count + 1).content_write(content)
   redirect '/'
 end
 
 get '/' do
-  @number = Dir.open('./public/memos').children.count
+  @number = Memo.memos_count
   erb :top
 end
 
 get '/edition/:i' do
   @number = params[:i]
-  @file_content = File.open("./public/memos/#{@number}.txt").read
+  @file_content = Memo.new(@number).memo_content
   erb :edition
 end
 
 patch '/memo/:i' do
   number = params[:i]
   content = params[:content]
-  File.open("./public/memos/#{number}.txt", 'wb') do |f|
-    f.write(content)
-  end
+  Memo.new(number).content_write(content)
   redirect '/'
 end
 
 get '/memo/:i' do
   @number = params[:i]
-  @file_content = File.open("./public/memos/#{@number}.txt").read
+  @file_content = Memo.new(@number).memo_content
   erb :memo
 end
 
 delete '/memo/:id' do
   number = params[:id]
-  File.delete("./public/memos/#{number}.txt")
+  Memo.new(number).delete
   redirect '/'
 end
